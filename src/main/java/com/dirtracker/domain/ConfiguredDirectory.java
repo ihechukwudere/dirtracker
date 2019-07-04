c48692399905ae640f7aec1e10f425a086205686
ihechukwudere <ihechukwudere@yahoo.com>
2019-07-04 13:24:25 +0300

    Updated function parameters

--- src/main/java/com/dirtracker/domain/ConfiguredDirectory.java
+++ src/main/java/com/dirtracker/domain/ConfiguredDirectory.java
@@ -1,9 +1,11 @@
 package com.dirtracker.domain;
 
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
 
 public class ConfiguredDirectory extends Directory {
 
-	public ConfiguredDirectory(String dirPath) {
+	public ConfiguredDirectory(Path dirPath) throws NoSuchFileException  {
 		super(dirPath);
 	}
 
