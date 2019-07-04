94a1d491f3863881d6211cb911b91664599da51f
ihechukwudere <ihechukwudere@yahoo.com>
2019-07-04 13:26:59 +0300

    Updated function parameters

--- src/main/java/com/dirtracker/DirectoryTrackerApplication.java
+++ src/main/java/com/dirtracker/DirectoryTrackerApplication.java
@@ -8,11 +8,20 @@ import com.dirtracker.service.ScheduleTaskService;
 @SpringBootApplication
 public class DirectoryTrackerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(DirectoryTrackerApplication.class, args);
-		
+	
 		ScheduleTaskService confDirTask = new ScheduleTaskService();
 		confDirTask.scheduleRepeatableCheckForConfiguredDirectory(3000, 0);
+		
+		/* To schedule a task that periodically checks 
+		 * a directory and read new xml configuration files
+		 * 
+		 * 1. Create a directory.
+		 * 2. Set the view type for displaying the file content(root entity tag) and property (file name).
+		 * 3. Set a reader (in this case an XML reader)
+		 * 4. 
+		 */
 	}
 
 }
--- src/main/java/com/dirtracker/service/ScheduleTaskService.java
+++ src/main/java/com/dirtracker/service/ScheduleTaskService.java
@@ -1,17 +1,48 @@
 package com.dirtracker.service;
 
+import java.math.BigDecimal;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import com.dirtracker.domain.ConfiguredDirectory;
 import com.dirtracker.domain.Directory;
+import com.dirtracker.task_scheduler.PeriodicallyReadNewConfigFileInDirectory;
 import com.dirtracker.task_scheduler.Task;
+import com.dirtracker.viewresolver.ConsoleView;
+import com.dirtracker.viewresolver.ContentReader;
+import com.dirtracker.viewresolver.ContentViewResolver;
+import com.dirtracker.viewresolver.xml.SAXStreamer;
 
 public class ScheduleTaskService {
 
 	private Directory directory;
 	
-	public void scheduleRepeatableCheckForConfiguredDirectory(long timeInterval, long delay) {
+	public void scheduleRepeatableCheckForConfiguredDirectory(long timeInterval, long delay)  {
 		String dirPath = "src/main/resources/configured_directory";
-		directory = new ConfiguredDirectory(dirPath);
-		Task scheduledTask = new DirectoryRepeatableCheck(directory);
+		
+		try {
+			directory = setConfiguredDirectoryPath(Paths.get(dirPath));
+			directory.setContentView(new ConsoleView());
+			createTask(new PeriodicallyReadNewConfigFileInDirectory(directory), timeInterval, delay);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public Directory setConfiguredDirectoryPath(Path dirPath) throws NoSuchFileException {
+		return directory = new ConfiguredDirectory(dirPath);
+	}
+	
+	private ContentViewResolver setContentViewResolver(ContentReader reader) {
+		ContentViewResolver consoleView = new ConsoleView();
+		consoleView.setContentReader(reader);
+		return consoleView;
+	}
+	
+	private void createTask(Task scheduledTask, long timeInterval, long delay) {
 		scheduledTask.scheduleTask(timeInterval, delay);
 	}
+	
+	
 }
