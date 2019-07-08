345aaf69d4d240bafec49643b3d2f964c9ec29bb
ihechukwudere <ihechukwudere@yahoo.com>
2019-07-08 19:12:52 +0300

    Added custom exception

---
+++ src/main/java/com/dirtracker/exception_handlers/InvalidFileExtensionException.java
@@ -0,0 +1,13 @@
+package com.dirtracker.exception_handlers;
+
+public class InvalidFileExtensionException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public InvalidFileExtensionException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
