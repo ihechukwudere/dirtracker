8efc5e0a622d394eabcd4f40c93011f7ffac4b55
ihechukwudere <ihechukwudere@yahoo.com>
2019-07-09 11:13:40 +0300

    Add null check before console display

--- src/main/java/com/dirtracker/viewresolver/ConsoleView.java
+++ src/main/java/com/dirtracker/viewresolver/ConsoleView.java
@@ -1,13 +1,19 @@
 package com.dirtracker.viewresolver;
 
 import java.nio.file.Path;
+import java.util.List;
+
+import com.dirtracker.domain.FileResource;
 
 public class ConsoleView extends ContentViewResolver {
 
+	List<FileResource<? extends Object>> fileResourceContainer;
+	
 	@Override
 	public void displayFileContent(Path filePath) throws Exception {
 		super.displayFileContent(filePath);
-		if(fileContentReader.readFile(filePath) != null) {
+		fileResourceContainer = fileContentReader.readFile(filePath);
+		if( fileResourceContainer != null) {
 			System.out.println(fileContentReader.readFile(filePath));
 		}
 		
